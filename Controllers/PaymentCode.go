257eabefe5eb42a8b6e7a87db4a816bdc6ac5525
e <e>
2020-04-03 22:45:04 +0800

    fix  return  url

--- Controllers/PaymentCode.go
+++ Controllers/PaymentCode.go
@@ -8,16 +8,17 @@ import (
 
 func GetOnePaymentCode(c *gin.Context) {
 	order_code := c.PostForm("order_code")
-	code_slice := map[string]bool{"wechat": true,"alipay": true}
+	code_slice := map[string]bool{"wechat": true, "alipay": true}
 	if _, ok := code_slice[order_code]; !ok {
 		ApiHelpers.RespondJSON(c, 0, "", "order_code参数非法")
 		return
 	}
 
 	var payment_code Models.PaymentCode
-	err := Models.GetOnePaymentCode(&payment_code,order_code)
+	err := Models.GetOnePaymentCode(&payment_code, order_code)
 	if err != nil {
-		ApiHelpers.RespondJSON(c, 0,payment_code , err.Error())
+		payment_code.img = "http://express.zhang6.net" + payment_code.img
+		ApiHelpers.RespondJSON(c, 0, payment_code, err.Error())
 	} else {
 		ApiHelpers.RespondJSON(c, 200, payment_code, "success")
 	}
