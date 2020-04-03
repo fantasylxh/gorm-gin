81acf9465721e81f9be2355cbbcdcd8f11c75787
e <e>
2020-04-03 22:37:00 +0800

    updateprder code

--- Models/PaymentCode.go
+++ Models/PaymentCode.go
@@ -6,7 +6,7 @@ import (
 )
 
 func GetOnePaymentCode(b *PaymentCode, code string) (err error) {
-	if err = Config.DB.Where("status =1 and code = ?", code).Order("id desc").First(b).Error; err != nil {
+	if err = Config.DB.Where("status =1 and order_code = ?", code).Order("id desc").First(b).Error; err != nil {
 		return err
 	}
 	return nil
